3e7cccbceb880ef9bc6ab9ca94cc79a38085b88b
DragooCho <66540054+DragooCho@users.noreply.github.com>
2020-12-13 19:58:48 +0900

    add

--- main.js
+++ main.js
@@ -7,3 +7,27 @@ ctx.canvas.height = ROWS * BLOCK_SIZE;
 
 // 블록의 크기를 변경한다.
 ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
+
+class Board {
+  grid;
+
+  // 새 게임이 시작되면 보드를 초기화한다.
+  reset() {
+    this.grid = this.getEmptyBoard();
+  }
+
+  // 0으로 채워진 행렬을 얻는다.
+  getEmptyBoard() {
+    return Array.from({ length: ROWS }, () => Array(COLS).fill(0));
+  }
+}
+
+let board = new Board();
+
+function play() {
+  board = getEmptyBoard();
+  let piece = new Piece(ctx);
+  piece.draw();
+
+  board.piece = piece;
+}
